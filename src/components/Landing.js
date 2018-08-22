c3e920390ec8a1b3ea287b9a5f876ff2b16578c3
Cortney Thomas <thomas.cortney@outlook.com>
2018-08-22 14:12:16 -0400

    Checkpoint 3 Routing: completed

---
+++ src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Landing extends React.Component {
+    render() {
+        return <section className='landing'>Landing page will go here</section>
+    }
+}
+
+export default Landing;
