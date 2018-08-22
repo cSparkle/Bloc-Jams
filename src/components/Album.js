6cdf9776a8b42fd690fcbfec8fdc98049e4786f2
Cortney Thomas <thomas.cortney@outlook.com>
2018-08-22 14:18:40 -0400

    Assignment 3 Routing: completed

---
+++ src/components/Album.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Album extends React.Component {
+    render() {
+        return <section className='album'>Album page will go here</section>
+    }
+}
+
+export default Album;
